56bab56921010a861cf7a3892b517a61267520f5
Carlos K Villanueva <carloskvillanueva@gmail.com>
2022-07-16 11:38:58 -0300

    Pokemon App - Vue Learning + dist

--- src/components/PokemonImage.vue
+++ src/components/PokemonImage.vue
@@ -1,11 +1,12 @@
 <template>
 	<div class="pokemon-container">
 
-		<img :src="imgSrc"
+		<img v-if="!showPokemon"
+			 :src="imgSrc"
 			 alt="pokemon"
 			 class="hidden-pokemon">
 
-		<img v-if="showPokemon"
+		<img v-else
 			 :src="imgSrc"
 			 alt="pokemon"
 			 class="fade-in">
--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -26,16 +26,19 @@ ul {
 	list-style-type: none;
 }
 li {
-	background-color: white;
+	background-color: #23487d;
+	color: white;
 	border-radius: 5px;
 	border: 1px solid rgba(0, 0, 0, 0.2);
 	cursor: pointer;
 	margin-bottom: 10px;
 	width: 250px;
+	padding: 5px;
+	text-transform: capitalize;
 }
 
 li:hover {
-	background-color: rgba(0, 0, 0, 0.05);
+	background-color: #132949;
 }
 
 .options-container {
